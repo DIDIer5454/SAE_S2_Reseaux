52c8fae6ad90fd27bcc08ce41935760981cefa76
Rafael Didier Menene Akoo Obono <menene12.akoo@gmail.com>
2022-06-08 15:48:34 +0200

    httpserverreconfig

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -9,7 +9,9 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HttpServer {
 
@@ -19,40 +21,35 @@ public class HttpServer {
     public HttpServer() {
     }
 
-    public static void toString(NodeList Zut) {
+    public static String toString(NodeList Zut) {
+        String S = "";
         for (int i = 0; i < Zut.getLength(); i++) {
-            System.out.println(Zut.item(i).getNodeName()+": "+Zut.item(i).getTextContent());
+            S +=  Zut.item(i).getTextContent();
         }
+        return S;
     }
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, TransformerException {
         DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document document = documentBuilder.parse("fxml/webconfig.xml");
 
-        NodeList ligne;//=document.getDocumentElement().getTextContent();
-        ligne = document.getDocumentElement().getElementsByTagName("port");
-        //String [] tab=ligne.split("\t");
-        System.out.println(ligne.item(0).getTextContent());
 
-        Node port = document.getDocumentElement();
+        NodeList Nport = document.getElementsByTagName("port");
 
-            NodeList root = document.getElementsByTagName("root");
-            NodeList index = document.getElementsByTagName("index");
-            NodeList accept = document.getElementsByTagName("accept");
-            NodeList reject = document.getElementsByTagName("reject");
+        NodeList root = document.getElementsByTagName("root");
+        NodeList index = document.getElementsByTagName("index");
+        NodeList accept = document.getElementsByTagName("accept");
+        NodeList reject = document.getElementsByTagName("reject");
 
-        System.out.println("port :" + port.getTextContent());
-        toString(root);
-        toString(index);
-        toString(accept);
-        toString(reject);
+        String Sport = toString(Nport);
+        String Sroot = toString(root);
+        String Sindex = toString(index);
+        String Saccept = toString(accept);
+        String Sreject = toString(reject);
 
 
-
-       /* if (args.length == 1) {
-            port = Integer.parseInt(args[0]);
-        }
-
+        port = Integer.parseInt(Sport);
+        
 
 
         while (true) {
@@ -61,7 +58,7 @@ public class HttpServer {
             boolean Sontour = false;
             boolean stop = false;
             String fichier = null;
-            stop=false;
+            stop = false;
             while (!stop) {
                 if (Sontour) {
 
@@ -71,8 +68,8 @@ public class HttpServer {
 
                         // ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 
-                        DataOutputStream dataOutputStream=new DataOutputStream(socket.getOutputStream());
-                        byte[]rep=reponse.getBytes();
+                        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                        byte[] rep = reponse.getBytes();
                         InputStream fileInputStream = new FileInputStream(fichier);
                         byte[] data = fileInputStream.readAllBytes();
                         socket.getOutputStream().write(rep);
@@ -83,15 +80,14 @@ public class HttpServer {
 
                         System.out.println("non trouve");
                     }
-                    stop=true;
-                }
-                else {
+                    stop = true;
+                } else {
                     System.out.println("[server]En attente du client");
                     InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
                     BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                     String recu = bufferedReader.readLine();
                     System.out.println(recu);
-                    if(recu!=null) {
+                    if (recu != null) {
                         fichier = "site web" + recu.substring(4, recu.indexOf("HTTP/1.1"));
                         System.out.println(fichier);
                     }
@@ -102,7 +98,7 @@ public class HttpServer {
 
             }
             serverSocket.close();
-        }*/
+        }
     }
 }
 
