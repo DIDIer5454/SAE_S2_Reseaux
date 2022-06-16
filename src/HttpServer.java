cbd309769c82ef4543e98b2ffb074a17eda9612f
Rafael Didier Menene Akoo Obono <menene12.akoo@gmail.com>
2022-06-16 16:40:34 +0200

    sd

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,10 +1,7 @@
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.print.attribute.Attribute;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,11 +9,15 @@ import javax.xml.transform.TransformerException;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+
 
 public class HttpServer {
 
-
+    public static boolean indexB;
     public static int port = 80;
+    public static ArrayList<String> rejette;
+    public static ArrayList<String> accepte;
 
     public HttpServer() {
     }
@@ -25,11 +26,182 @@ public class HttpServer {
         String S = "";
         for (int i = 0; i < Zut.getLength(); i++) {
             S += Zut.item(i).getTextContent();
+            if (Zut.getLength() > 1) {
+                S += "\n";
+            }
         }
         return S;
     }
 
+    /**
+     * methode convertissant un entier en tableau de 8 entier representant les 8 bit dun octet
+     *
+     * @param i entier
+     * @return
+     */
+    public static int[] conversionbit(int i) {
+        int[] binaries = {128, 64, 32, 16, 8, 4, 2, 1};
+        int[] reseau = {0, 0, 0, 0, 0, 0, 0, 0};
+        for (int p = 0; p < reseau.length; p++) {
+            if (i - binaries[p] >= 0) {
+                i = i - binaries[p];
+                reseau[p] = 1;
+            }
+        }
+        return reseau;
+    }
+
+    /**
+     * methode convertissant un tableau de 8 entier representant des bit doctet
+     * en entier
+     *
+     * @param i
+     * @return
+     */
+    public static int convertionOctet(int[] i) {
+        int octet = 0;
+        int[] binaries = {128, 64, 32, 16, 8, 4, 2, 1};
+        for (int k = 0; k < i.length; k++) {
+            if (i[k] == 1) {
+                octet += binaries[k];
+            }
+        }
+        return octet;
+    }
+
+    /**
+     * methode retournant un tableau a double entree de bit
+     * representant les bit du masque
+     *
+     * @param mask
+     * @return
+     */
+    public static int[][] faireBitMask(int mask) {
+        int nb1 = 0;
+        int k1 = 0;
+        int k2 = 0;
+        int[][] reseaumaskBit = new int[4][8];
+        while (nb1 < mask) {
+            reseaumaskBit[k1][k2] = 1;
+            nb1++;
+            k2++;
+            if (k2 > 7) {
+                k2 = 0;
+                k1++;
+            }
+        }
+        return reseaumaskBit;
+    }
+
+    /**
+     * methode fesant une addition bit a bit
+     *
+     * @param Ip
+     * @param mask
+     * @return
+     */
+    public static int[] addition(int[] Ip, int mask) {
+        int[][] reseauBit = new int[4][8];
+        int[][] addressBit = new int[4][8];
+
+        int[] addressReseau = new int[4];
+
+        int[][] reseaumaskBit = faireBitMask(mask);
+
+        for (int k = 0; k < Ip.length; k++) {
+            for (int l = 0; l < reseauBit[k].length; l++) {
+                reseauBit[k][l] = conversionbit(Ip[k])[l];
+
+                if (reseauBit[k][l] == 1 && reseaumaskBit[k][l] == 1) {
+                    addressBit[k][l] = 1;
+                } else {
+                    addressBit[k][l] = 0;
+                }
+            }
+            addressReseau[k] = convertionOctet(addressBit[k]);
+        }
+        return addressReseau;
+    }
+
+    /**
+     * methode verfiant si l'addresse ip coorespond a une addresse de l'adresse reseau en parametre
+     *
+     * @param tab
+     * @param Ip
+     * @return
+     */
+    public static boolean estDans(String[] tab, String Ip) {
+        boolean m = true;
+
+        /*
+-currentAddressMask=tableau de String de la forme {"Adrresse Reseau" ;"Mask"}
+-addressReseauString est l'AdrressReseau sous forme de chaine de caractère
+-mask cest le masque reseau sous forme d'entier
+-currentIP est laddresse IP client sous forme de tableau dentier de taille 4 (ce programme ne traite que les addresse en IPv4
+            ,sauf si lhote du client  est aussi lordinateur hebergeant le serveur).
+
+ **la logique de cette methode est de rechercher une addresse reseau a partir de laddresse Ip du client
+ et du masque du reseau cette addresse calculé sera comparé a laddresse reseau fourni par le tableau en
+ parametre si cest identique alors la methode renvoi true.
+
+ */
+        for (int j = 0; j < tab.length; j++) {
+
+            String[] currentAddressMask = tab[j].split("/");
+            String[] addressReseauString = currentAddressMask[0].split("\\.");
+            int mask = Integer.parseInt(currentAddressMask[1]);
+            String[] currentIP = Ip.split("\\.");
+
+            int[] addressIP = new int[4];
+            int[] addressReseau = new int[4];
+            /*
+            remplissage des tableau addressIP et addressReseau
+             */
+            for (int i = 0; i < currentIP.length; i++) {
+                addressIP[i] = Integer.parseInt(currentIP[i]);
+                addressReseau[i] = Integer.parseInt(addressReseauString[i]);
+            }
+            /*
+            addition de ladresse Ip et du mask
+             */
+            int[] reseauIP = addition(addressIP, mask);
+            /*
+             verfication avec un parcours partiel
+             */
+            int k = 0;
+            while (m == true && k < 4) {
+                m = reseauIP[k] == addressReseau[k];
+                k++;
+            }
+        }
+
+        return m;
+    }
+
+    /**
+     * methode retournant un texte html
+     * repertoriant tout les fichier et dossier présent
+     * dans le repertoire root du site
+     * avec les liens correspondant
+     *
+     * @return
+     */
+    public String genererPremier() {
+        String texte = "";
+
+        return texte;
+    }
+
+
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, TransformerException {
+
+        System.clearProperty("java.net.preferIPv6Addresses");
+        System.clearProperty("preferIPv4Stack");
+
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        System.setProperty("java.net.preferIPv6Addresses", "false");
+
+
         DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document document = documentBuilder.parse("fxml/webconfig.xml");
 
@@ -46,16 +218,42 @@ public class HttpServer {
         String Sindex = toString(index);
 
         String Saccept = toString(accept);
-
         String Sreject = toString(reject);
 
 
         port = Integer.parseInt(Sport);
+        indexB = Boolean.parseBoolean(Sindex);
+        String[] rejecttab = Sreject.split("\n");
+        String[] accepttab = Saccept.split("\n");
 
 
         while (true) {
             ServerSocket serverSocket = new ServerSocket(port);
+
             Socket socket = serverSocket.accept();
+            String address = "" + socket.getInetAddress();
+            /*
+            traitement de laddresse ip pour etre exploitable dans la methode estDAns()
+             */
+            address = address.substring(1);
+            if (address.contains(":")) {
+                address = address.replace(':', '.');
+
+                address = address.substring(8);
+            }
+            System.out.println(address);
+            System.out.println(serverSocket.getLocalSocketAddress());
+            if (estDans(rejecttab, address)) {
+                socket.close();
+                serverSocket.close();
+            }
+           /*if (accepttab.length > 0) {
+                if (!estDans(accepttab, address)) {
+                    socket.close();
+                    serverSocket.close();
+                }
+            }*/
+
             boolean Sontour = false;
             boolean stop = false;
             String fichier = null;
@@ -81,6 +279,7 @@ public class HttpServer {
                     }
                     stop = true;
                 } else {
+
                     System.out.println("[server]En attente du client");
                     InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
                     BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
--- src/HttpServerTest.java
+++ src/HttpServerTest.java
@@ -1,4 +1,44 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class HttpServerTest {
-  
+    @Test
+    public void Testconversionbit() {
+        int[] IP = {192, 168, 12, 1};
+        int[][] Bit = {{1, 1, 0, 0, 0, 0, 0, 0}, {1, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1}};
+        int[] reseau;
+        for (int i = 0; i < Bit.length; i++) {
+            reseau = HttpServer.conversionbit(IP[i]);
+            assertArrayEquals(Bit[i], reseau);
+        }
+    }
+
+    @Test
+    public void TestconversionOctet() {
+        int[] IP = {192, 168, 12, 1};
+        int[][] Bit = {{1, 1, 0, 0, 0, 0, 0, 0}, {1, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1}};
+        for (int i = 0; i < Bit.length; i++) {
+            for (int j = 0; j < Bit[i].length; j++) {
+                int octet = HttpServer.convertionOctet(Bit[i]);
+                assertEquals(octet,IP[i]);
+            }
+        }
+    }
+    @Test
+    public void TestAddition(){
+        int[] address={192,168,12,0};
+        int[] IP = {192, 168, 12, 1};
+        int [] resultat=HttpServer.addition(IP,24);
+        assertArrayEquals(address,resultat);
+    }
+
+    @Test
+    public void TestEstDans(){
+        boolean m;
+        String[]tab={"192.168.12.0/24"};
+        assertTrue(HttpServer.estDans(tab,"192.168.12.1"));
+        assertFalse(HttpServer.estDans(tab,"192.168.13.2"));
+        assertFalse(HttpServer.estDans(tab,"180.123.12.1"));
+    }
 }
